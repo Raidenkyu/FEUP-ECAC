888883b6bb4fde11bb0f354ed94c1dfee00c23af
João Carlos Maduro <joao-carlos.14@hotmail.com>
2020-09-28 12:49:49 +0100

    Finished data parsing

--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,1 @@
-"""Analysis module"""
 import analysis
--- src/analysis.py
+++ src/analysis.py
@@ -1,14 +1,34 @@
-"""Import Path"""
 from pathlib import Path
-
 import pandas as pd
 
-EXAMPLE_FILE = 'account.csv'
 
-folder_path = Path("res/")
+def read_dataset(dataset_name):
+    folder_path = Path("res/")
 
-FILE_PATH = str(folder_path / EXAMPLE_FILE)
+    file_path = str(folder_path / dataset_name)
+    dataset = pd.read_csv(file_path, sep=";")
 
-dataset = pd.read_csv(FILE_PATH, sep=";")
+    return dataset
 
-print(dataset)
+
+def parse_data():
+    account_dataset = read_dataset("account.csv")
+    client_dataset = read_dataset("client.csv")
+    disp_dataset = read_dataset("disp.csv")
+    district_dataset = read_dataset("district.csv")
+
+    return account_dataset, client_dataset, disp_dataset, district_dataset
+
+
+def parse_train():
+    loan_train_dataset = read_dataset("loan_train.csv")
+    trans_train_dataset = read_dataset("trans_train.csv")
+
+    return loan_train_dataset, trans_train_dataset
+
+
+def parse_test():
+    loan_test_dataset = read_dataset("loan_test.csv")
+    trans_test_dataset = read_dataset("trans_test.csv")
+
+    return loan_test_dataset, trans_test_dataset
