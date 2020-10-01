08fb2c8121d640445edc0bad1549544858a643fd
João Carlos Maduro <joao-carlos.14@hotmail.com>
2020-10-01 16:27:51 +0100

    Started preprocessing

--- src/__main__.py
+++ src/__main__.py
@@ -2,5 +2,7 @@ import database as db
 import pandas as pd
 
 account_dataset, client_dataset, disp_dataset, district_dataset = db.parse_data()
+loan_train_dataset, trans_train_dataset = db.parse_train()
+loan_test_dataset, trans_test_dataset = db.parse_test()
 
-print(db.select_account(576))
+print(loan_train_dataset)
