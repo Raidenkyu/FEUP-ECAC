2b6e2c794904c61ee4f6189802e03605b3a7333c
João Carlos Maduro <joao-carlos.14@hotmail.com>
2020-10-07 20:32:12 +0100

    Added data joining operations

--- src/__main__.py
+++ src/__main__.py
@@ -16,7 +16,8 @@ account_dataset, client_dataset, disp_dataset, district_dataset = db.parse_data(
 loan_train_dataset, trans_train_dataset = db.parse_train()
 loan_test_dataset, trans_test_dataset = db.parse_test()
 
-loan_train_dataset = prepare_dataset(loan_train_dataset)
+loan_train_dataset = prepare_dataset(
+    loan_train_dataset, disp_dataset, account_dataset, district_dataset, client_dataset)
 
 train, test = train_test_split(
     loan_train_dataset, test_size=0.25, random_state=42, shuffle=True
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,2 +1,28 @@
-def prepare_dataset(train_dataset):
-    return train_dataset
+import pandas as pd
+
+
+def prepare_dataset(train_dataset, disp, account, district, client):
+    joined_client = disp.set_index("client_id", drop=False).join(
+        client.set_index("client_id"), rsuffix='_other')
+
+    joined_account = joined_client.set_index("account_id", drop=False).join(
+        account.set_index("account_id"), rsuffix='_other')
+
+    joined_district = joined_account.set_index("district_id", drop=False).join(
+        district.set_index("code "), rsuffix='_other')
+
+    joined = joined_district.set_index("account_id", drop=False).join(
+        train_dataset.set_index("account_id", drop=False), rsuffix='_other')
+
+    joined = joined.drop(
+        columns=["disp_id", "client_id", "account_id", "district_id",
+                 "district_id_other", "account_id_other", "date_other"]
+    ).dropna(subset=["loan_id"]).set_index("loan_id")
+
+    joined = pd.get_dummies(joined[~joined.index.duplicated(keep='first')])
+
+    for col in joined.columns:
+        print(col)
+
+    print(joined)
+    return joined
