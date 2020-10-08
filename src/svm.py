b6b08036f95ba55d7db223d6104aee0371470be8
João Carlos Maduro <joao-carlos.14@hotmail.com>
2020-10-09 00:05:47 +0100

    Fixed SVM

--- src/svm.py
+++ src/svm.py
@@ -7,10 +7,10 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 
 def svm_loan(train_dataset, test_dataset, eval_dataset):
-    X_test = test_dataset.iloc[:, 0:5].values
-    y_test = test_dataset.iloc[:, 6].values
-    X_train = train_dataset.iloc[:, 0:5].values
-    y_train = train_dataset.iloc[:, 6].values
+    X_test = test_dataset.iloc[:, 0:21].values
+    y_test = test_dataset.iloc[:, 22].values
+    X_train = train_dataset.iloc[:, 0:21].values
+    y_train = train_dataset.iloc[:, 22].values
 
     scaler = StandardScaler()
     scaler.fit(X_train)
@@ -26,12 +26,15 @@ def svm_loan(train_dataset, test_dataset, eval_dataset):
     print(str(confusion_matrix(y_test, y_pred)))
     print(str(classification_report(y_test, y_pred)))
 
-    X_eval = eval_dataset.iloc[:, 0:5].values
+    X_eval = eval_dataset.iloc[:, 0:21].values
     X_eval = scaler.transform(X_eval)
+
+    id_array = map(lambda x: int(x), eval_dataset.index.values)
+    y_pred = map(lambda x: int(x), svclassifier.predict(X_eval))
 
     y_pred = svclassifier.predict(X_eval)
     result = pd.DataFrame({
-        'Id': eval_dataset.iloc[:, 0].values,
+        'Id': id_array,
         'Predicted': y_pred
     })
 
