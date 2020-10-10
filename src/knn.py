5469a98eca7f7f7f0f11173132852bf37578f457
João Carlos Maduro <joao-carlos.14@hotmail.com>
2020-10-10 15:28:15 +0100

    Replaced error with AUC

--- src/knn.py
+++ src/knn.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
@@ -19,16 +20,17 @@ def knn_loan(train_dataset, test_dataset, eval_dataset):
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
 
-    error = []
+    auc = []
 
     # Calculating error for K values between 1 and 40
     for i in range(1, 40):
         knn = KNeighborsClassifier(n_neighbors=i)
         knn.fit(X_train, y_train)
         pred_i = knn.predict(X_test)
-        error.append(np.mean(pred_i != y_test))
+        fpr, tpr, _thresholds = metrics.roc_curve(y_test, pred_i)
+        auc.append(metrics.auc(fpr, tpr))
 
-    best_k = error.index(min(error)) + 1
+    best_k = auc.index(max(auc)) + 1
 
     knn = KNeighborsClassifier(n_neighbors=best_k)
     knn.fit(X_train, y_train)
